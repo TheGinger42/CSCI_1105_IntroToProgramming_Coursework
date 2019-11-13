5707c7473c3b14675dd0917682e5e75972776114
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2019-11-13 14:32:10 -0700

    Add files via upload

--- Ch1/Exercise1_5.java
+++ Ch1/Exercise1_5.java
@@ -6,6 +6,6 @@ public class Exercise1_5 {
 
 		This program displays the result of an equasion.*/
 
-		System.out.println((9.5*4.5-2.5*3/45.5-3.5));
+		System.out.println((((9.5*4.5)-(2.5*3))/(45.5-3.5)));
 	}
 }
