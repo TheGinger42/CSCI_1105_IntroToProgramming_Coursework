f3e4fbee7be4bbb32ae262a79f2179bc6c777fe2
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2019-11-13 15:20:02 -0700

    Add files via upload

---
+++ ch2/Exercise2_1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercise2_1 {
+	public static void main(String[] args) {
+		/* 
+		Author: Troy Aiken
+		Date: 11/14/2019
+		
+		covert Celcius to Fahrenheit.*/
+		
+
+		
+		//create Scanner
+		Scanner input = new Scanner(System.in);
+			
+		//prompt user for degree in Celcius
+		System.out.print("Enter a number for Celcuis: ");
+		double celcius = input.nextDouble();
+		
+		//compute Fahrenheit
+		double fahrenheit = (9.0 / 5) * celcius + 32;
+		
+		//display results
+		System.out.println(celcius + " degrees Celcius is " + fahrenheit + " degrees Fahrenheit");
+			
+	}
+}
