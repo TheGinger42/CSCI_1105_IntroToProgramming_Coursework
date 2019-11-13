c369862a4331ad0391a687d3293ef28d61c6aa53
Troy <faolan42@gmail.com>
2019-11-13 14:16:08 -0700

    created and uploaded 3 assignments.

---
+++ Exercise1_9.java
@@ -0,0 +1,13 @@
+/*
+Author: Troy Aiken
+Date: 11/13/2019
+
+This program displays the result of an equasion.*/
+
+public class Exercise1_9 {
+	public static void main(String[] args) {
+
+	System.out.println(4.5 * 7.9);	
+	System.out.println((4.5 * 2) + (7.9 * 2));
+	}
+}
