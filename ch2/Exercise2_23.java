fd545459ee4fc47d5d57c2952bcb4a7b1a131a6f
404kevnotfound <student@HSiMac4-10.local>
2019-11-14 13:17:08 -0700

    created program for calculating the cost of driving.

---
+++ ch2/Exercise2_23.java
@@ -0,0 +1,24 @@
+/* 
+Author: Troy Aiken
+Date: 11/14/2019
+class: Introduction to Programming*/
+
+
+import java.util.Scanner;
+
+public class Exercise2_23 {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the driving distance in miles: ");
+		double distance = input.nextDouble();
+		System.out.print("Enter miles per gallon: ");
+		double mpg = input.nextDouble();
+		System.out.print("Enter price per Gallon: ");
+		double ppg = input.nextDouble();
+		double cost = (distance / mpg) * ppg;
+		
+		System.out.println("The cost of driving is " + cost);
+	}
+}
