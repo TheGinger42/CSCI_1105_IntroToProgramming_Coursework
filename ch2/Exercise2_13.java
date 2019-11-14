95d06ab2ffa99d2bcbe17a2966e42a7b66c5894d
404kevnotfound <student@HSiMac4-10.local>
2019-11-14 12:57:32 -0700

    finished compound interest.

--- ch2/Exercise2_13.java
+++ ch2/Exercise2_13.java
@@ -1,25 +1,38 @@
 /* 
 Author: Troy Aiken
 Date: 11/14/2019
-
+Class: Introduction to Programing
 Financial application: compound value*/
 
 import java.util.Scanner;
 import javax.xml.xpath.*;
 import java.time.temporal.*;
+import javax.print.attribute.standard.*;
 
 public class Exercise2_13 {
 		public static void main(String[] args) {
-		//create Scanner
+	
 		Scanner input = new Scanner(System.in);
-		// variables
-		interest = (1 + 0.00417);
+	
+		double interest = (1 + 0.00417);
 		
-		//prompt user to input monthly savings amount.
 		System.out.print("Input your monthly saving amount: ");
+		
 		double msa = input.nextDouble();
+		double newBalance = 0.0;
+		double oldBalance = 0.0;
+		newBalance = msa * interest;
+		oldBalance = newBalance;
+		newBalance = (msa + oldBalance) * (interest);
+		oldBalance = newBalance;
+		newBalance = (msa + oldBalance) * (interest);
+		oldBalance = newBalance;
+		newBalance = (msa + oldBalance) * (interest);
+		oldBalance = newBalance;
+		newBalance = (msa + oldBalance) * (interest);
+		oldBalance = newBalance;
+		newBalance = (msa + oldBalance) * (interest);
 		
-		//equasion
-		double av = msa * interest;
-		
-		System.out.println("After the sixth month, the account value is $" + av);
+		System.out.println("After the sixth month, the account value is $" + newBalance);
+		}
+}
