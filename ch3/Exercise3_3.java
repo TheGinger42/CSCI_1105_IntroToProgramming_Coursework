f6a16f68a2a69bea70c4d4203915cd2b1305e78f
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2019-11-18 13:45:43 -0700

    Add files via upload
    No matter what I did, I continue to get this error line
    Exercise3_3.java:26: error: 'else' without 'if'
    		else { 
    		^
    I had Stacy help me, and she says it should work as it is. So we are stuck on this.

---
+++ ch3/Exercise3_3.java
@@ -0,0 +1,33 @@
+/* 
+Author: Troy Aiken
+Date: 11/18/19
+class: Introduction to Programming
+using variables and user input to solve an equation. */
+
+import java.util.Scanner;
+
+public class Exercise3_3 {
+	public static void main(String[] args) {
+	
+	Scanner input = new Scanner(System.in);
+	System.out.print("Enter A, B, C, D, E, F: ");
+	double a = input.nextDouble();
+	double b = input.nextDouble();
+	double c = input.nextDouble();
+	double d = input.nextDouble();
+	double e = input.nextDouble();
+	double f = input.nextDouble();
+	
+	double h = ((a * d) - (b * c));
+	
+	if (h == 0); {
+		System.out.println("Cannot divide by 0");
+	}
+		else { 
+		double x = (((e * d) - (b * f)) / h);
+		double y = (((a * f) - (e * c)) / h);
+		System.out.println("X = " + x);
+		System.out.println("Y = " + y);
+	}
+	}
+} 
