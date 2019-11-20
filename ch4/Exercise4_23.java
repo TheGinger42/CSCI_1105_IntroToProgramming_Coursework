2503b97baf8dcdc4cfabf0773a24463445960331
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2019-11-20 12:55:06 -0700

    Update Exercise4_23.java
    added overtime

--- ch4/Exercise4_23.java
+++ ch4/Exercise4_23.java
@@ -17,23 +17,26 @@ public class Exercise4_23 {
 		double hours = input.nextDouble();
 		System.out.print("Enter hourly pay rate: ");
 		double rate = input.nextDouble();
-		System.out.print("Enter federal tax withholding rate: ");
-		double ftax = input.nextDouble();
-		System.out.print("Enter State tax withholding rate: ");
-		double stax = input.nextDouble();
-		double gross = hours * rate;
+		double ftax = .2;
+		double stax = .05;
+		double oT = hours - 40;
+		double oTrate = rate * 1.5;
+		double gross = (hours * rate) + (oT * oTrate);
 		double fedTax = gross * ftax;
 		double stateTax = gross * stax;
 		double net = gross - (fedTax + stateTax); 
 		double deduction = stateTax + fedTax;
 		
 		System.out.println("Employee Name: " + emp);
-		System.out.println("Hours Worked: " + hours);
+		System.out.println("Regular Hours Worked: " + hours);
+		System.out.println("Overtime hours: " + oT);
 		System.out.println("Pay Rate: " + rate);
+		System.out.println("Overtime Pay Rate: " + oTrate);
 		System.out.printf("Gross Pay %.2f\n", gross);
 		System.out.println("Deductions:");
-		System.out.printf("	Federal Withholding: %.2f%%\n", fedTax);
-		System.out.printf("	State Withholding: %.2f%%\n", stateTax);
-		System.out.printf("	Total Deduction: %.2f\n", deduction);
+		System.out.printf("	Federal Withholding: $%.2f\n", fedTax);
+		System.out.printf("	State Withholding: $%.2f\n", stateTax);
+		System.out.printf("	Total Deduction: $%.2f\n", deduction);
 		System.out.printf("Net Pay: %.2f", net);
+	}
 }
