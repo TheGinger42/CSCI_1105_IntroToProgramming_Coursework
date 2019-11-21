414d5502b678fc0c09418aba96a1b830a10beda1
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2019-11-21 15:47:00 -0700

    Add files via upload

---
+++ ch6/Exercise6_17.java
@@ -0,0 +1,24 @@
+/*
+Author: Troy Aiken
+Date: 11/21/19
+Class: introduction to programming
+define and call a matrix.
+*/
+
+import java.util.Scanner;
+class Exercise6_17 {
+	public static void main(String[] args) {
+		int num = (int)(Math.random() * 2);
+		
+		Scanner input = new Scanner(System.in);
+		//System.out.print("enter a number: ");
+		int n = 3; //input.nextInt();
+				
+		for (int numbers_per_line = 0; numbers_per_line < n; numbers_per_line++){
+				
+		}
+		System.out.print(num);
+	
+		
+	}
+}
