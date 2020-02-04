9b1d2fb890c7a702a4587b5b57bcb29b509258ec
TheGinger42 <57685002+TheGinger42@users.noreply.github.com>
2020-02-04 14:35:54 -0700

    Update Chapter13_11.java

--- Ch 13/Exercise 11/Chapter13_11.java
+++ Ch 13/Exercise 11/Chapter13_11.java
@@ -10,7 +10,7 @@ package octagon;
 public class Chapter13_11 {
 	public static void main(String[] args) {
 	Octagon shape1 = new Octagon(4);
-	Octagon shape2 = shape1;
+	Octagon shape2 = shape1.clone();
 	System.out.println(shape1.compareTo(shape2));
 	System.out.println("The area of the Octagon is: " + shape1.getArea());
 	}
